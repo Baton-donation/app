def19d9251316c929b9ae236799660836afcf914
Max Isom <hi@maxisom.me>
2021-05-03 18:24:22 -0500

    Update consent text

--- renderer/components/consent.tsx
+++ renderer/components/consent.tsx
@@ -4,12 +4,38 @@ import { Typography, Paper } from "@material-ui/core";
 const ConsentText = () => (
   <Paper style={{ maxHeight: "50vh", overflow: "scroll", padding: "1rem" }}>
     <Typography gutterBottom>
-      You are being invited to participate in a research study that aims to
-      collect sentences written by users of Augmentative and Alternative
-      Communication (AAC) interfaces. These sentences will help improve AAC text
-      entry interfaces. Contributed sentences will become part of a public data
-      set. This study is being conducted by Dr. Keith Vertanen from the
-      Department of Computer Science at Michigan Technological University.
+      You are invited to take part in a research study collecting sentences
+      written by users of Augmentative and Alternative Communication (AAC)
+      interfaces. Contributed sentences will become part of a public data set.
+      This data set will be available for download by researchers working to
+      improve AAC text entry interfaces.
+    </Typography>
+
+    <Typography gutterBottom component="span">
+      This study is being conducted by:
+      <ul>
+        <li>
+          Dr. Keith Vertanen from the Department of Computer Science at Michigan
+          Technological University. Dr. Vertanen is an expert on text input by
+          people with diverse abilities. He contributes to the open-source AAC
+          text entry interfaces Dasher and Nomon. He has been a consultant for
+          Tobii Dynavox, helping to improve gaze-based text input. He is a
+          recipient of a National Science Foundation (NSF) CAREER award to
+          investigate context-aware AAC text input.
+        </li>
+        <li>
+          Will Wade, PGDip, Bsc (Hons) is a Senior AAC Consultant at the Ace
+          Centre. He leads access assessments for individuals across the
+          northwest of England. He teaches on a Masters course in Assistive
+          Technology at Manchester Metropolitan University.
+        </li>
+        <li>
+          Owen Kent is an entrepreneur and researcher focusing on assistive
+          technologies. A graduate of UC Berkeley, he has over seven years of
+          experience advising engineering teams ranging from high school
+          students to postdocs building technology for people with disabilities.
+        </li>
+      </ul>
     </Typography>
 
     <Typography gutterBottom component="span">
@@ -24,16 +50,19 @@ const ConsentText = () => (
           You must use an AAC interface that logs your writing to a simple text
           file (such as Dasher)
         </li>
+        <li>You must not have a cognitive impairment</li>
       </ul>
     </Typography>
 
     <Typography gutterBottom>
-      If you agree to take part, you will install our application on your
+      If you agree to take part, you will install our custom application on your
       computer. You will point the application at a text file of things you have
       written using your AAC interface. You can then select whatever sentences
       you would like to share with the project. You can share as many sentences
       as you would like. You can return to our application whenever you would
-      like to share more sentences.
+      like to share more sentences. The application only runs upon request; it
+      does not run in the background or otherwise monitor your day-to-day
+      computer activities. You can uninstall the application at any time.
     </Typography>
 
     <Typography gutterBottom component="span">
@@ -71,42 +100,49 @@ const ConsentText = () => (
     </Typography>
 
     <Typography gutterBottom>
-      We review your data for any privacy compromising information before we add
-      it to our public data set. We only add sentences to our public data set
+      The study team periodically reviews your uploaded data for any privacy
+      compromising information. We only add sentences to our public data set
       that you have uploaded more than a month ago. This gives you time to
-      remove sentences before they become part of the public data set.
+      remove sentences before they become part of the public data set. We will
+      honor requests to retroactively remove sentences from the public data, but
+      copies may exist that are outside of our control.
     </Typography>
 
     <Typography gutterBottom>
-      We will honor requests to retroactively remove sentences from the public
-      data, but copies may exist that are outside of our control.
+      Before we upload your data, we encrypt it for anonymous delivery to our
+      cloud server. This process uses our project&apos;s public encryption key.
+      Our private key is needed to read any data encrypted with our public key.
+      We store our private key separately on a non-networked device. This
+      ensures even if our cloud server were hacked, your uploaded data cannot be
+      read. We periodically retrieve data from the cloud server, decrypting it
+      on our local computer. For each sentence you upload, our application
+      stores a unique random ID on your computer. This unique ID allows you to
+      delete any sentences you may have uploaded accidentally.
     </Typography>
 
     <Typography gutterBottom>
-      There are no known risks if you decide to participate in this research
-      study. There are no costs to you for participating in the study. You will
-      not immediately benefit from taking part. However, your efforts may lead
-      to improvements to the interfaces used by you and others. You have the
-      right to delete any of the data you contribute.
+      The only known risk to taking part in this research study is that you
+      might accidentally share sentences containing private information. We
+      believe the risk of this information becoming public is small given how we
+      store and review your data. There are no costs to you for participating in
+      the study. You will not immediately benefit from taking part. However,
+      your efforts may lead to improvements to the interfaces used by you and
+      others. You have the right to delete any of the data you contribute.
     </Typography>
 
     <Typography gutterBottom>
-      Your participation in this study is voluntary. By clicking the Accept box
-      below, you are voluntarily agreeing to participate. You are also agreeing
-      that your sentences may be used in a public data set. You are free to skip
-      any parts of this study without penalty.
+      Your participation in this study is voluntary. By clicking &quot;Agree and
+      continue&quot;, you are voluntarily agreeing to take part. You agree that
+      your contributed data may be used in a public data set. You are free to
+      skip any parts of this study without penalty.
     </Typography>
 
     <Typography gutterBottom>
       If you have any questions about this study, please contact Dr. Keith
-      Vertanen at (906) 487-2331 or vertanen@mtu.edu.
-    </Typography>
-
-    <Typography gutterBottom>
-      The Michigan Tech Institutional Review Board (IRB) has reviewed my request
-      to conduct this project. If you have any concerns about your rights in
-      this study, please contact Michigan Tech&apos;s IRB at (906) 487-2902 or
-      irb@mtu.edu.
+      Vertanen at (906) 487-2331 or vertanen@mtu.edu. The Michigan Tech
+      Institutional Review Board (IRB) has reviewed my request to conduct this
+      project. If you have any concerns about your rights in this study, please
+      contact Michigan Tech&apos;s IRB at (906) 487-2902 or irb@mtu.edu.
     </Typography>
   </Paper>
 );
